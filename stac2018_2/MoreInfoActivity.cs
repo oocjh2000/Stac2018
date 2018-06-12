57b9cd4745baf4f1f516096e9322745b031a33d0
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-06-12 23:13:46 +0900

    파일이름변경

--- stac2018_2/MoreInfoActivity.cs
+++ stac2018_2/MoreInfoActivity.cs
@@ -24,7 +24,20 @@ namespace stac2018_2
             SetContentView(Resource.Layout.MoreInfoLayout);
             UserIfoButton = FindViewById<ImageButton>(Resource.Id.UserInfoButton);
             VideoCallButton = FindViewById<ImageButton>(Resource.Id.ViedoCallButton);
+
+            UserIfoButton.Click += UserIfoButton_Click;
+            VideoCallButton.Click += VideoCallButton_Click;
             // Create your application here
+        }
+
+        private void VideoCallButton_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(VideoCallActivity));
+        }
+
+        private void UserIfoButton_Click(object sender, EventArgs e)
+        {
+           StartActivity(typeof(MainActivity));
         }
     }
 }
