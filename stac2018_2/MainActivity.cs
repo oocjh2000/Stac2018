14a70bd463e8b5be0abfa2d3c23b8bb836d83066
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-06-12 09:08:20 +0900

    초기커밋

--- stac2018_2/MainActivity.cs
+++ stac2018_2/MainActivity.cs
@@ -2,19 +2,61 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Graphics.Drawables;
+using Android.Graphics;
+using Android.Content;
 
 namespace stac2018_2
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        ImageButton MoreOptionButton;
+        ImageButton VideoCallButton;
+        ImageButton UserInfoButton;
+        Drawable BackGround;
+        Drawable BackGroundWhite;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            BackGround = new ColorDrawable(Color.ParseColor("#3F51B5"));
+            BackGroundWhite = new ColorDrawable(Color.ParseColor("#FFFFFF"));
+
+            VideoCallButton = FindViewById<ImageButton>(Resource.Id.ViedoCallButton);
+            MoreOptionButton = FindViewById<ImageButton>(Resource.Id.MoreOptionButton);
+            UserInfoButton = FindViewById<ImageButton>(Resource.Id.UserInfoButton);
+
+            VideoCallButton.Click += VideoCallButton_Click;
+            UserInfoButton.Click += UserInfoButton_Click;
+            MoreOptionButton.Click += MoreOptionButton_Click;
         }
+
+        private void UserInfoButton_Click(object sender, System.EventArgs e)
+        {
+            UserInfoButton.SetImageResource(Resource.Drawable.userinfowhite);
+            UserInfoButton.Background = (BackGround);
+            var intent = new Intent(this, typeof(UserInfoActivity));
+            StartActivity(intent);
+            OverridePendingTransition(0, 0);
+        }
+
+        private void VideoCallButton_Click(object sender, System.EventArgs e)
+        {
+            VideoCallButton.SetImageResource(Resource.Drawable.videocallwhite);
+            VideoCallButton.Background = BackGround;
+        }
+
+        private void MoreOptionButton_Click(object sender, System.EventArgs e)
+        {
+            MoreOptionButton.SetImageResource(Resource.Drawable.moreoptionwhite);
+            MoreOptionButton.Background = (BackGround);
+        }
+
+        
     }
 }
 
