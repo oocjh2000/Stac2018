193b3c9c10291e70aa354bc27f5bcd224944a005
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-06-16 20:11:10 +0900

    한박자 쉬고

---
+++ stac2018_2/CodeFile1.cs
@@ -0,0 +1,31 @@
+﻿using Android;
+using Android.Widget;
+using Android.Views;
+using Android.Graphics.Drawables;
+using Java.Lang;
+using System.Collections.Generic;
+
+public class VideoCallAdapter : BaseAdapter
+{
+     List<VideoCallItem> videoCallItems = new List<VideoCallItem>();
+
+    public VideoCallAdapter() { }
+
+
+    public override int Count => throw new System.NotImplementedException();
+
+    public override Object GetItem(int position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override long GetItemId(int position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override View GetView(int position, View convertView, ViewGroup parent)
+    {
+        throw new System.NotImplementedException();
+    }
+}
